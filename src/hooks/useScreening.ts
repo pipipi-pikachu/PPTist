221af9b6e7828ddf0fb0fe3414e0d838edaf07e8
claviering <claviering@gmail.com>
2022-05-24 14:47:36 +0800

    fix: detect immediately full screen is not work after entering full screen #106

--- src/hooks/useScreening.ts
+++ src/hooks/useScreening.ts
@@ -8,7 +8,9 @@ export default () => {
   // 进入放映状态（从当前页开始）
   const enterScreening = () => {
     enterFullscreen()
-    screenStore.setScreening(true)
+    setTimeout(() => {
+      screenStore.setScreening(true)
+    }, 200)
   }
 
   // 进入放映状态（从第一页开始）
