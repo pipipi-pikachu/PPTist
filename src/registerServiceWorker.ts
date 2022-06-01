61e5b42eb4af94d7827ad1b8d2cc005ef00f0d07
pipipi-pikachu <pipipi_pikachu@163.com>
2022-06-01 20:33:57 +0800

    chore: update serviceWorker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -4,11 +4,8 @@ import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+    ready(registration) {
+      registration.update()
     },
     registered() {
       console.log('Service worker has been registered.')
