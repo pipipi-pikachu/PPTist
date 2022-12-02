757b38c0d67fe9d1f607bf19376cd16db58d8d24
pipipi-pikachu <pipipi_pikachu@163.com>
2022-12-02 10:35:09 +0800

    chore: 删除 ttf 字体相关

--- scripts/build-fonts.js
+++ scripts/build-fonts.js
@@ -1,59 +1,45 @@
+// 自动获取 src\assets\fonts 路径下的字体文件列表，并替换相关的 SCSS 变量
+
+/* eslint-disable no-console */
+
 const fs = require('fs')
-const ttf2woff2 = require('ttf2woff2')
 const path = require('path')
+
 const assetsPath = path.join(__dirname, '../src/assets')
-const scssVariablePath = path.join(assetsPath, 'styles/variable.scss')
+const fontVarPath = path.join(assetsPath, 'styles/font.scss')
 const fontsPath = path.join(assetsPath, 'fonts')
+
 const scssFontListVar = '$fontList'
 
-// scss 文件中 $fontList 变量替换
-const replaceSCSSFile = (names) => {
-  fs.readFile(scssVariablePath, 'utf-8', (errs, content) => {
+const replaceSCSSVariable = (names) => {
+  fs.readFile(fontVarPath, 'utf-8', (errs, content) => {
     if (errs) {
       console.error(errs)
       process.exit(1)
     }
     const reg = new RegExp(`(\\${scssFontListVar}:\\s*)(.*)(;.*)`, 'g')
     const newContent = content.replace(reg, `$1${names}$3`)
-    fs.writeFile(scssVariablePath, newContent, (errs) => {
+    fs.writeFile(fontVarPath, newContent, errs => {
       if (errs) {
         console.error(errs)
         process.exit(1)
       }
-      console.log('替换 SCSS 字体列表成功！')
+      console.log('自动生成自定义字体列表完成')
     })
   })
 }
 
-// ttf 转 woff2
 fs.readdir(fontsPath, { withFileTypes: true }, (errs, files) => {
   if (errs) {
     console.error(errs)
     process.exit(1)
   }
-  const fontList = []
-  files
-    .filter(({ name }) => name.endsWith('.ttf'))
-    .forEach(({ name }) => {
-      console.log(`${name} 正在转换为 woff2 格式...`)
-      const woff2FontExists = files.find((item) => item.name.endsWith('.woff2'))
-      const replacedName = name.replace('.ttf', '.woff2')
-      fontList.push(name.replace('.ttf', ''))
-      if (woff2FontExists) {
-        console.log(`${replacedName} 已存在，不再转换`)
-        return
-      }
+  const woff2Fonts = files.filter(({ name }) => name.endsWith('.woff2'))
+  const fontList = woff2Fonts.map(({ name }) => name.replace('.woff2', ''))
 
-      const input = fs.readFileSync(path.join(fontsPath, name))
-      fs.writeFileSync(
-        path.join(fontsPath, replacedName),
-        ttf2woff2(input)
-      )
-      console.log(`${name} 转换 woff2 成功！`)
-    })
   const names = fontList.reduce((result, name, i) => {
     if (i === 0) return `'${name}'`
     return `${result}, '${name}'`
   }, '')
-  replaceSCSSFile(names)
+  replaceSCSSVariable(names)
 })
