86b6ffe044f70883d75747131dd3fa6eb7ea6fef
pipipi-pikachu <pipipi_pikachu@163.com>
2023-09-14 22:01:54 +0800

    perf: style fix

--- src/views/Editor/SelectPanel.vue
+++ src/views/Editor/SelectPanel.vue
@@ -262,12 +262,13 @@ const close = () => {
 }
 .input {
   width: 100%;
-  height: 18px;
-  line-height: 18px;
+  height: 16px;
   border: 0;
   outline: 0;
   padding-left: 0;
   padding-right: 0;
   flex: 1;
+  font-size: 12px;
+  background-color: transparent;
 }
 </style>
