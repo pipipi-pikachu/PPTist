17ab32a4395a6ac5a4faefc8c7404e0df84da32d
pipipi-pikachu <pipipi_pikachu@163.com>
2024-03-09 14:43:32 +0800

    fix: 临时处理（导入PPTX文件带来的异常数据，#255）

--- src/hooks/useExport.ts
+++ src/hooks/useExport.ts
@@ -490,6 +490,7 @@ export default () => {
         else if (el.type === 'shape') {
           if (el.special) {
             const svgRef = document.querySelector(`.thumbnail-list .base-element-${el.id} svg`) as HTMLElement
+            if (svgRef.clientWidth < 1 || svgRef.clientHeight < 1) continue // 临时处理（导入PPTX文件带来的异常数据）
             const base64SVG = svg2Base64(svgRef)
 
             const options: pptxgen.ImageProps = {
