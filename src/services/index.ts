b61b770ca15ba1371c2ed8ff2319b341b8944fa4
pipipi-pikachu <pipipi_pikachu@163.com>
2025-01-03 17:56:36 +0800

    chore: 字体文件托管到云空间

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,6 @@ import axios from './config'
 
 export default {
   getMockData(filename: string): Promise<any> {
-    return axios.get(`/mocks/${filename}.json`)
+    return axios.get(`./mocks/${filename}.json`)
   },
 }
