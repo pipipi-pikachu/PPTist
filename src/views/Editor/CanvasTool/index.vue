df8e093ab24ce71912b618ab49ba65ff10e58dcc
pipipi-pikachu <pipipi_pikachu@163.com>
2025-11-15 23:57:33 +0800

    perf: 画布工具条UI优化

--- src/views/Editor/CanvasTool/index.vue
+++ src/views/Editor/CanvasTool/index.vue
@@ -20,8 +20,8 @@
     </div>
 
     <div class="add-element-handler">
-      <div class="handler-item group-btn" v-tooltip="'插入文字'">
-        <IconFontSize class="icon" :class="{ 'active': creatingElement?.type === 'text' }" @click="drawText()" />
+      <div class="insert-handler-item group-btn" :class="{ 'active': creatingElement?.type === 'text' }" v-tooltip="'插入文字'">
+        <div class="group-btn-main" @click="drawText()"><IconFontSize class="icon" /> <span class="text">文本框</span></div>
         
         <Popover trigger="click" v-model:value="textTypeSelectVisible" style="height: 100%;" :offset="10">
           <template #content>
@@ -31,12 +31,12 @@
           <IconDown class="arrow" />
         </Popover>
       </div>
-      <div class="handler-item group-btn" v-tooltip="'插入形状'" :offset="10">
+      <div class="insert-handler-item group-btn" :class="{ 'active': creatingCustomShape || creatingElement?.type === 'shape' }" v-tooltip="'插入形状'" :offset="10">
         <Popover trigger="click" style="height: 100%;" v-model:value="shapePoolVisible" :offset="10">
           <template #content>
             <ShapePool @select="shape => drawShape(shape)" />
           </template>
-          <IconGraphicDesign class="icon" :class="{ 'active': creatingCustomShape || creatingElement?.type === 'shape' }" />
+          <div class="group-btn-main"><IconGraphicDesign class="icon" /> <span class="text">形状</span></div>
         </Popover>
         
         <Popover trigger="click" v-model:value="shapeMenuVisible" style="height: 100%;" :offset="10">
@@ -47,9 +47,9 @@
           <IconDown class="arrow" />
         </Popover>
       </div>
-      <div class="handler-item group-btn" v-tooltip="'插入图片'">
+      <div class="insert-handler-item group-btn" v-tooltip="'插入图片'">
         <FileInput style="height: 100%;" @change="files => insertImageElement(files)">
-          <IconPicture class="icon" />
+          <div class="group-btn-main"><IconPicture class="icon" /> <span class="text">图片</span></div>
         </FileInput>
         
         <Popover trigger="click" v-model:value="imageMenuVisible" style="height: 100%;" :offset="10">
@@ -66,13 +66,17 @@
         <template #content>
           <LinePool @select="line => drawLine(line)" />
         </template>
-        <IconConnection class="handler-item" :class="{ 'active': creatingElement?.type === 'line' }" v-tooltip="'插入线条'" />
+        <div class="insert-handler-item" :class="{ 'active': creatingElement?.type === 'line' }" v-tooltip="'插入线条'">
+          <IconConnection class="icon" /> <span class="text">线条</span>
+        </div>
       </Popover>
       <Popover trigger="click" v-model:value="chartPoolVisible" :offset="10">
         <template #content>
           <ChartPool @select="chart => { createChartElement(chart); chartPoolVisible = false }" />
         </template>
-        <IconChartProportion class="handler-item" v-tooltip="'插入图表'" />
+        <div class="insert-handler-item" v-tooltip="'插入图表'">
+          <IconChartProportion class="icon" /> <span class="text">图表</span>
+        </div>
       </Popover>
       <Popover trigger="click" v-model:value="tableGeneratorVisible" :offset="10">
         <template #content>
@@ -81,9 +85,13 @@
             @insert="({ row, col }) => { createTableElement(row, col); tableGeneratorVisible = false }"
           />
         </template>
-        <IconInsertTable class="handler-item" v-tooltip="'插入表格'" />
+        <div class="insert-handler-item" v-tooltip="'插入表格'">
+          <IconInsertTable class="icon" /> <span class="text">表格</span>
+        </div>
       </Popover>
-      <IconFormula class="handler-item" v-tooltip="'插入公式'" @click="latexEditorVisible = true" />
+      <div class="insert-handler-item" v-tooltip="'插入公式'" @click="latexEditorVisible = true">
+        <IconFormula class="icon" /> <span class="text">公式</span>
+      </div>
       <Popover trigger="click" v-model:value="mediaInputVisible" :offset="10">
         <template #content>
           <MediaInput 
@@ -92,9 +100,13 @@
             @insertAudio="({ src, ext }) => { createAudioElement(src, ext); mediaInputVisible = false }"
           />
         </template>
-        <IconVideoTwo class="handler-item" v-tooltip="'插入音视频'" />
+        <div class="insert-handler-item" v-tooltip="'插入音视频'">
+          <IconVideoTwo class="icon" /> <span class="text">音视频</span>
+        </div>
       </Popover>
-      <IconSymbol class="handler-item" :class="{ 'active': showSymbolPanel }" v-tooltip="'插入符号'" @click="toggleSymbolPanel()" />
+      <div class="insert-handler-item" :class="{ 'active': showSymbolPanel }" v-tooltip="'插入符号'" @click="toggleSymbolPanel()">
+        <IconSymbol class="icon" /> <span class="text">符号</span>
+      </div>
     </div>
 
     <div class="right-handler">
@@ -109,7 +121,7 @@
           >{{item}}%</PopoverMenuItem>
           <PopoverMenuItem center @click="resetCanvas(); canvasScaleVisible = false">适应屏幕</PopoverMenuItem>
         </template>
-        <span class="text">{{canvasScalePercentage}}</span>
+        <span class="text">{{ canvasScalePercentage }}</span>
       </Popover>
       <IconPlus class="handler-item viewport-size" v-tooltip="'画布放大（Ctrl + =）'" @click="scaleCanvas('+')" />
       <IconFullScreen class="handler-item viewport-size-adaptation" v-tooltip="'适应屏幕（Ctrl + 0）'" @click="resetCanvas()" />
@@ -292,46 +304,62 @@ const openImageLibPanel = () => {
   transform: translate(-50%, -50%);
   display: flex;
 
-  .handler-item {
-    width: 32px;
+  & > div {
+    flex-shrink: 0;
+  }
+
+  .insert-handler-item {
+    height: 30px;
+    font-size: 14px;
+    margin: 0 2px;
+    padding: 0 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: $borderRadius;
+    overflow: hidden;
+    cursor: pointer;
 
     &:not(.group-btn):hover {
       background-color: #f1f1f1;
     }
 
     &.active {
-      color: $themeColor;
+      background-color: #f1f1f1;
+    }
+
+    .icon {
+      margin-right: 4px;
     }
 
     &.group-btn {
-      width: auto;
-      margin-right: 5px;
+      margin-right: 6px;
+      padding: 0;
 
       &:hover {
         background-color: #f3f3f3;
       }
 
-      .icon, .arrow {
+      .group-btn-main {
         height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
-      }
-
-      .icon {
-        width: 26px;
-        padding: 0 2px;
+        padding: 0 5px;
 
         &:hover {
           background-color: #e9e9e9;
         }
-        &.active {
-          color: $themeColor;
-        }
       }
-      .arrow {
-        font-size: 12px;
 
+      .arrow {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 12px;
+        padding: 0 1px;
+  
         &:hover {
           background-color: #e9e9e9;
         }
@@ -380,6 +408,27 @@ const openImageLibPanel = () => {
   }
 }
 
+@media screen and (width <= 1600px) {
+  .add-element-handler {
+    .insert-handler-item {
+      padding: 0 10px;
+
+      .icon {
+        margin-right: 0;
+      }
+      .text {
+        display: none;
+      }
+    }
+  }
+}
+@media screen and (width <= 1366px) {
+  .add-element-handler {
+    .insert-handler-item {
+      padding: 0 6px;
+    }
+  }
+}
 @media screen and (width <= 1200px) {
   .right-handler .text {
     display: none;
@@ -389,12 +438,6 @@ const openImageLibPanel = () => {
   }
   .more-icon {
     display: block;
-  }
-  .add-element-handler {
-    .handler-item {
-      width: 30px;
-      margin: 0 1px;
-    }
   }
 }
 @media screen and (width <= 1000px) {
