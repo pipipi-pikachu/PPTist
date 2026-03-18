9973418ab1ed71ebe76c1099e737a4dd0aea5ae5
pipipi-pikachu <pipipi_pikachu@163.com>
2026-03-18 20:54:56 +0800

    perf: 优化放映键盘翻页（#431）

--- src/views/Screen/hooks/useExecPlay.ts
+++ src/views/Screen/hooks/useExecPlay.ts
@@ -256,7 +256,7 @@ export default () => {
   }
 
   // 快捷键翻页
-  const keydownListener = (e: KeyboardEvent) => {
+  const keydownListener = throttle(function(e: KeyboardEvent) {
     const key = e.key.toUpperCase()
 
     if (key === KEYS.UP || key === KEYS.LEFT || key === KEYS.PAGEUP) execPrev()
@@ -267,7 +267,7 @@ export default () => {
       key === KEYS.ENTER ||
       key === KEYS.PAGEDOWN
     ) execNext()
-  }
+  }, 500, { leading: true, trailing: false })
 
   onMounted(() => {
     if (!isAudienceMode) document.addEventListener('keydown', keydownListener)
